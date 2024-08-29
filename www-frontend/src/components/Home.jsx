f95672fdd2c93ebac1dbd9d8fbd30caac725254b
MartinHargous <mmhargous@miuandes.cl>
2024-08-29 19:54:44 -0400

    creacion de componentes

---
+++ www-frontend/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './Home.css'
+
+function Home() {
+
+  return (
+    <>
+      <div>
+        <p>Hola</p>
+      </div>
+    </>
+  )
+}
+
+
+export default App
