7380221acf48d4d7ea910edba320fd34fd3951f1
MartinHargous <mmhargous@miuandes.cl>
2024-10-16 20:16:56 -0300

    minor fixes

--- hybrid-frontend/components/Events.jsx
+++ hybrid-frontend/components/Events.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-
+import { View, Text, StyleSheet } from 'react-native';
+import Header from "./Header";
 const Events = () => {
   return (
+    <View style={styles.container1}>
+      <Header/>
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Aquí va Events</Text>
     </View>
+    </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container1: {
+    flex: 1,
+    padding: 0,
+    backgroundColor: '#525277',
+  },});
 export default Events;
